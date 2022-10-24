08684ffe6f4d3fc64aed86066b58588a143a4071
Al8x <rohacho2020@gmail.com>
2022-10-24 17:12:36 +0300

    create template

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css'
+
+const AppInfo = () => {
+    return(
+        <div className='app-info'>
+            <h1>Employee accounting in N-company</h1>
+            <h2>Total number: </h2>
+            <h2>Bonus will receive: </h2>
+        </div>
+    );
+}
+
+export default AppInfo;
